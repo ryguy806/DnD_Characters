7d6d3f65dc36bb05f85175a611fafd41158454bd
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-06-26 20:26:10 -0400

    Began the home page and updated the index.

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+//Error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload
+require_once('vendor/autoload.php');
+
+//Creates the instance of the base class
+$f3 = Base::instance();
+
+//Specified the default route
+$f3->route('GET /', function ($f3) {
+
+
+    $template = new Template();
+    echo $template->render('views/home.html');
+});
+
+$f3->run();
