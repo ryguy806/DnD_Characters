14d393c95cff2253acc759a96c37e1a93439b2e3
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-06-26 21:25:38 -0400

    updated index, homepage, and started first character creation site.

--- index.php
+++ index.php
@@ -12,9 +12,26 @@ $f3 = Base::instance();
 //Specified the default route
 $f3->route('GET /', function ($f3) {
 
+    if(isset($_SERVER['REQUEST_METHOD'])){
 
+        if() {
+            $f3->reroute('start');
+        }
+    }
     $template = new Template();
     echo $template->render('views/home.html');
+});
+
+$f3->route('GET /start', function ($f3) {
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+
+        $f3->reroute('start');
+    }
+    $template = new Template();
+    echo $template->render('views/beginning.html');
 });
 
 $f3->run();
