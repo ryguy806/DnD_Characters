f8c5c36a34cf2d038a901098999355498bd8d8a2
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-06-27 20:14:22 -0400

    Created player class, added missing info to beginning, and updated index.

---
+++ classes/Player.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Class Player This class is the main holding area for the player's information.
+ */
+class Player
+{
+    private $_name;
+    private $_class;
+    private $_race;
+    private $_armor;
+    private $_background;
+    private $_weapons;
+    private $_shield;
+    private $_ac;
+    private $_str;
+    private $_dex;
+    private $_con;
+    private $_wis;
+    private $_int;
+    private $_cha;
+
+    /**
+     * Player constructor.
+     * @param $_name
+     * @param $_class
+     * @param $_race
+     * @param $_armor
+     * @param $_background
+     * @param $_weapons
+     * @param $shield
+     */
+    public function __construct($_name, $_class, $_race, $_armor, $_background, $_weapons, $shield)
+    {
+        $this->_name = $_name;
+        $this->_class = $_class;
+        $this->_race = $_race;
+        if ($_armor == 'Unarmored'){
+            $this->_armor = 0;
+        }
+        elseif ($_armor == 'Padded' || $_armor == 'Leather') {
+            $this->_armor = 11;
+        }
+        elseif ($_armor == 'Studded Leather' || $_armor == 'Hide') {
+            $this->_armor = 12;
+        }
+        elseif ($_armor == 'Chain shirt') {
+            $this->_armor = 13;
+        }
+        elseif ($_armor == 'Scale mail' || $_armor == 'Ring mail' || $_armor == 'Breastplate'){
+            $this->_armor = 14;
+        }
+        elseif ($_armor == 'Half plate') {
+            $this->_armor = 15;
+        }
+        elseif ($_armor == 'Chain mail') {
+            $this->_armor = 16;
+        }
+        elseif ($_armor == "Splint") {
+            $this->_armor = 17;
+        }
+        else{
+            $this->_armor = 18;
+        }
+        $this->_background = $_background;
+        $this->_weapons = $_weapons;
+        if($shield == 'yes'){
+            $this->_shield = 2;
+        }
+        else{
+            $this->_shield = 0;
+        }
+    }
+
+    public function calcAC(){
+        $this->_ac = $this->_shield + $this->_armor + $this->_dex;
+    }
+
+
+}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
 //Require the autoload
 require_once('vendor/autoload.php');
 
@@ -10,21 +12,39 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Specified the default route
-$f3->route('GET /', function ($f3) {
+$f3->route('GET|POST /', function ($f3) {
 
     if(isset($_SERVER['REQUEST_METHOD'])){
 
-        if() {
-            $f3->reroute('start');
+        $button = $_SESSION['help'];
+        if(isset($_SESSION['alone'])){
+            $button = $_SESSION['alone'];
+        }
+
+        $f3->set('button', $button);
+
+        if($button == 'alone') {
+            $f3->reroute('character');
+        }
+        else{
+            $f3->reroute('guide');
         }
     }
     $template = new Template();
     echo $template->render('views/home.html');
 });
 
-$f3->route('GET /start', function ($f3) {
+$f3->route('GET|POST /character', function ($f3) {
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $name = $_SESSION['name'];
+        $race = $_SESSION['name'];
+        $class = $_SESSION['name'];
+        $background = $_SESSION['background'];
+        $armor = $_SESSION['armor'];
+        $weapon = $_SESSION['weapon'];
+        $shield = $_SESSION['shield'];
 
 
 
@@ -32,6 +52,12 @@ $f3->route('GET /start', function ($f3) {
     }
     $template = new Template();
     echo $template->render('views/beginning.html');
+});
+
+$f3->route('GET|POST /guide', function ($f3) {
+
+    $template = new Template();
+    echo $template->render();
 });
 
 $f3->run();
