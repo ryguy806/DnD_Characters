65190e605860dc7675ec6f93de28b50b74cedecb
RyGuy8806 <L1ghtSab3r11!!>
2020-03-18 13:17:16 -0700

    Began routing to the creator site.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 //error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -12,8 +12,25 @@ $f3 = Base::instance();
 $characters=array();
 
 $f3->route('GET /', function (){
+
+
+
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+ $f3->route('GET /creator', function (){
+
+     if(!empty($_POST)){
+         $name = $_POST['name'];
+         $race = $_POST['race'];
+         $class = $_POST['class'];
+
+
+     }
+
+     $view = new Template();
+     echo $view->render('views/character_creator.html');
+ });
 
 $f3->run();
